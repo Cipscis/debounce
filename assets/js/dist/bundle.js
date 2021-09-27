3b5e9002f17856b6017fd91d2482cbcdc8925595
Cipscis <cipscis+github@gmail.com>
2021-09-27 00:24:46 +0000

    Deploying to gh-pages from @ Cipscis/debounce@2b867f0cd607b906337cd45df90fbb75a1abfd58 🚀

--- assets/js/dist/bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";const t=function(t,e){let c;return function(){c&&clearTimeout(c),c=setTimeout((()=>{c=void 0,t.apply(this,arguments)}),e)}},e=function(t){t.preventDefault();let e=t.target,c=parseInt(e.getAttribute("data-activate-count"));c+=1,e.setAttribute("data-activate-count",c)};document.querySelectorAll(".js-debounce-fast").forEach((c=>c.addEventListener("click",t(e,200)))),document.querySelectorAll(".js-debounce-slow").forEach((c=>c.addEventListener("click",t(e,1e3))))})();
-//# sourceMappingURL=bundle.js.map
